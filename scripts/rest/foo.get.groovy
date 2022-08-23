366d89bfc0a713e92426e0e70a39926394e45589
admin admin <evaladmin@example.com>
2022-08-22 21:52:39 -0400

    "User admin wrote content /scripts/rest/foo.get.groovy"

--- scripts/rest/foo.get.groovy
+++ scripts/rest/foo.get.groovy
@@ -13,48 +13,14 @@ import com.rometools.rome.feed.rss.Item;
 import com.rometools.rome.feed.synd.SyndPerson;
 import com.rometools.rome.io.SyndFeedOutput;
 
-    Channel channel = new Channel();
-    channel.setFeedType("rss_2.0");
-    channel.setTitle("HowToDoInJava Feed");
-    channel.setDescription("Different Articles on latest technology");
-    channel.setLink("https://howtodoinjava.com");
-    channel.setUri("https://howtodoinjava.com");
-    channel.setGenerator("In House Programming");
- 
-    Image image = new Image();
-    image.setUrl("https://howtodoinjava.com/wp-content/uploads/2015/05/howtodoinjava_logo-55696c1cv1_site_icon-32x32.png");
-    image.setTitle("HowToDoInJava Feed");
-    image.setHeight(32);
-    image.setWidth(32);
-    channel.setImage(image);
- 
-    Date postDate = new Date();
-    channel.setPubDate(postDate);
- 
-    Item item = new Item();
-    item.setAuthor("Lokesh Gupta");
-    item.setLink("https://howtodoinjava.com/spring5/webmvc/spring-mvc-cors-configuration/");
-    item.setTitle("Spring CORS Configuration Examples");
-    item.setUri("https://howtodoinjava.com/spring5/webmvc/spring-mvc-cors-configuration/");
-    item.setComments("https://howtodoinjava.com/spring5/webmvc/spring-mvc-cors-configuration/#respond");
- 
-    com.rometools.rome.feed.rss.Category category = new com.rometools.rome.feed.rss.Category();
-    category.setValue("CORS");
-    item.setCategories(Collections.singletonList(category));
- 
-    Description descr = new Description();
-    descr.setValue(
-        "CORS helps in serving web content from multiple domains into browsers who usually have the same-origin security policy. In this example, we will learn to enable CORS support in Spring MVC application at method and global level."
-            + "The post <a rel=\"nofollow\" href=\"https://howtodoinjava.com/spring5/webmvc/spring-mvc-cors-configuration/\">Spring CORS Configuration Examples</a> appeared first on <a rel=\"nofollow\" href=\"https://howtodoinjava.com\">HowToDoInJava</a>.");
-    item.setDescription(descr);
-    item.setPubDate(postDate);
-    
- 
-    channel.setItems(Collections.singletonList(item));
-    //Like more Entries here about different new topics
+SyndFeed feed = new SyndFeedImpl();
+feed.setFeedType("rss_1.0");
+feed.setTitle("Test title");
+feed.setLink("http://www.somelink.com");
+feed.setDescription("Basic description");
 
     
     
     SyndFeedOutput output = new SyndFeedOutput();
-  return output.outputString(channel, true);
+  return output.outputString(feed, true);
     
