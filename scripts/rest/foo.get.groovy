09a3ad6895d8391c17514b10a2046b3cc5a312fe
admin admin <evaladmin@example.com>
2022-08-22 21:54:33 -0400

    "User admin wrote content /scripts/rest/foo.get.groovy"

--- scripts/rest/foo.get.groovy
+++ scripts/rest/foo.get.groovy
@@ -1,18 +1,8 @@
 @Grab(group='rome', module='rome', version='1.0')
 
-import com.rometools.rome.feed.atom.Category;
-import com.rometools.rome.feed.atom.Content;
-import com.rometools.rome.feed.atom.Entry;
-import com.rometools.rome.feed.atom.Feed;
-import com.rometools.rome.feed.atom.Link;
-import com.rometools.rome.feed.atom.Person;
-import com.rometools.rome.feed.rss.Channel;
-import com.rometools.rome.feed.rss.Description;
-import com.rometools.rome.feed.rss.Image;
-import com.rometools.rome.feed.rss.Item;
-import com.rometools.rome.feed.synd.SyndPerson;
 import com.rometools.rome.io.SyndFeedOutput;
 import com.rometools.rome.feed.synd.SyndFeedImpl
+import com.rometools.rome.feed.synd.SyndFeed
 
 SyndFeed feed = new SyndFeedImpl();
 feed.setFeedType("rss_1.0");
