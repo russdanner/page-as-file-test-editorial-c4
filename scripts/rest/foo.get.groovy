9b22b43f0a28d49c289af18c9c5ab6b89919a573
admin admin <evaladmin@example.com>
2022-08-22 21:56:38 -0400

    "User admin wrote content /scripts/rest/foo.get.groovy"

--- scripts/rest/foo.get.groovy
+++ scripts/rest/foo.get.groovy
@@ -1,14 +1,24 @@
 @Grab(group='rome', module='rome', version='1.0')
 
 import com.rometools.rome.io.SyndFeedOutput;
-import com.rometools.rome.feed.synd.SyndFeedImpl
-import com.rometools.rome.feed.synd.SyndFeed
+import com.rometools.rome.feed.synd.*
 
 SyndFeed feed = new SyndFeedImpl();
 feed.setFeedType("rss_1.0");
 feed.setTitle("Test title");
 feed.setLink("http://www.somelink.com");
 feed.setDescription("Basic description");
+
+SyndEntry entry = new SyndEntryImpl();
+entry.setTitle("Entry title");        
+entry.setLink("http://www.somelink.com/entry1");
+
+SyndContent description = new SyndContentImpl();
+description.setType("text/html");
+description.setValue("First entry");
+entry.setDescription(description);
+    
+feed.setEntries(Arrays.asList(entry));
 
     
     
